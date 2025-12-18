8106411aaf8380757449c5e8a4d36c9e15d43b82
yarik <aroslavpavlivskij@gmail.com>
2025-12-18 23:37:07 +0200

    fix name

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import css from './App.module.css';
 
-import CafeInfo from '../CafeInfo/CafeInfo.tsx';
+import CafeInfo from '../CafeInfo/Cafe.tsx';
 import VoteOptions from '../VoteOptions/VoteOptions.tsx';
 import Notification from '../Notification/Notification.tsx';
 
